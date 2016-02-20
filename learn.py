82710c062394cb2dd24e7a20bb45c22462b26c98
quatridunull <quatridunull@gmail.com>
2016-02-20 17:50:40 -0500

    linode notes + some more learn.py edits

--- learn.py
+++ learn.py
@@ -7,8 +7,39 @@ import random
 import cv2
 import numpy as np
 
+#===================
+# class declarations
+#===================
 
+# model class - stores the model fit and centers
+class Model:
+	def __init__(self, score = 0, centers = []):
+		self.score = score
+		self.centers = []
+# end class model
 
+#=====================
+# prediction functions
+#=====================
+
+# from a model and an image, determine if that image contains nodules
+def predict(model,img):
+	predictions = []
+	# get img features
+	features = SIFT DETECTOR (____)
+	# compare each feature with model cluster centers
+	centerDistances = [];
+	centerValues = [];
+	for i in range(0,len(features)):
+		for j in range(0,len(model.centers)):
+			.... problem: which feature is the one we care about?
+	return predictions
+	
+# end predict
+
+#===================
+# training functions
+#===================
 
 # helper math evaluation stuff for test eval
 npa = np.array
@@ -19,9 +50,6 @@ def softmax(w,t=1.0):
 	dist = e/np.sum(e)
 	return dist
 # end softmax
-
-
-
 
 # takes in N*X*Y matrix of images
 # outputs split data
@@ -45,8 +73,25 @@ def split(list,numsplits):
 # <score, cluster-1-type, cluster-1-center, ....., cluster-n-type, cluster-n-center>
 # where each of the above vector elements is a list/array
 def train(splits,numsplits):
-	testSplit = 0
-	
+	models = []
+	for testSplit in range(0,numsplits):
+		currmodel = Model() # create a Model object
+		for i in range(0,numsplits):
+			if (i != testSplit):
+				for j in range(0,len(splits[i])): # for each img in split
+					# detect features with SIFT
+					# classify the features based on if they are located
+					# where we expect nodules to be
+		# k-means cluster all features
+		currmodel.clusters = kmeans......
+		# run and evaluate test data
+		weights = [];
+		for i in range(0,len(splits(testSplit)):
+			# test against model
+			
+		currmodel.score = softmax(weights)
+		models.append(currmodel)
+	return models
 # end train
 
 def main(list):
